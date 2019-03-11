f8c22efb2a1ed8c7937b3b2a5a1282c814f21c17
Aidan Drzewicki <02aidan.drzewicki@gulllakecs.org>
2019-03-11 09:51:09 -0400

    Add code to the project to get it started

--- final-project.py
+++ final-project.py
@@ -6,3 +6,32 @@
 # There will be multiple sentences with different areas the user can input
 # a word of their choice to make their own mini story.
 
+
+question = int(input('Hello, would you like to do madlib 1 or 2? '))
+try:
+    if question == (1):
+        madlib1()
+
+    if question == (2):
+        madlib2()
+
+except ValueError:
+    print('Please enter 1 or 2')
+
+
+def madlib(1):
+    print('Roses are ' + adj1 + '.')
+    print(n1 + 'are blue.')
+    print(n2 + ' are ' + adj2)
+    print('And so are you!')
+
+def madlib(2):
+    print('The ' + adj1 + n1 + 'fox jumped over a fence.')
+    print('Then he saw a ' + adj2 + n2 + ' coming towards him.')
+    print('He then decided to ' + adj3)
+
+adj1 = input('Tell me an adjective and press enter: ')
+n1 = input('Tell me a noun and press enter: ')
+n2 = input('Tell me another noun and press enter: ')
+adj2 = input('Tell me another adjective and press enter: ')
+adj3 = input('Tell me another adjective and press enter: ')
