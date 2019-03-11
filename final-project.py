ff41cb7fd9e961d049ceaf589438277c46e251d2
Aidan Drzewicki <02aidan.drzewicki@gulllakecs.org>
2019-03-11 09:05:33 -0400

    Add project to the origin master in terminal

--- final-project.py
+++ final-project.py
@@ -2,3 +2,7 @@
 # 3.7.19
 # Final Project
 
+# Mad libs generator
+# There will be multiple sentences with different areas the user can input
+# a word of their choice to make their own mini story.
+
